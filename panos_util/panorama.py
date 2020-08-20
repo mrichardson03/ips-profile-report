a266ffc25fb5b2c731d4e8d8ce9acaffdb08c15d
Michael Richardson <mrichardson03@gmail.com>
2020-08-19 21:17:40 -0400

    refactor: move device group parsing logic

--- panos_util/panorama.py
+++ panos_util/panorama.py
@@ -3,12 +3,18 @@ from xml.etree import ElementTree
 
 import xmltodict
 
-from .objects import DefaultVulnerabilityProfile, StrictVulnerabilityProfile
+from .objects import (
+    DefaultVulnerabilityProfile,
+    SecurityProfileGroup,
+    StrictVulnerabilityProfile,
+    VulnerabilityProfile,
+)
 from .policies import SecurityRule
 
 
 class DeviceGroup:
-    def __init__(self, rules, vuln_profiles, profile_groups):
+    def __init__(self, name, rules, vuln_profiles, profile_groups):
+        self.name = name
         self.rules = rules
         self.vuln_profiles = {
             "strict": StrictVulnerabilityProfile(),
@@ -57,14 +63,31 @@ class DeviceGroup:
         return self._lookup_profile(pg.vulnerability)
 
     @staticmethod
-    def parse_rules(xml):
+    def create_from_xml(xml):
         tree = ElementTree.fromstring(xml)
-        rules = []
 
-        for rule in tree.findall("./entry"):
-            print(rule)
-            xmldict = xmltodict.parse(ElementTree.tostring(rule))
+        dg = tree.find(".")
+        name = dg.attrib["name"]
+
+        vuln_profiles = {}
+        for vuln_profile in dg.findall("./profiles/vulnerability/entry"):
+            raw_xml = ElementTree.tostring(vuln_profile)
+            xmldict = xmltodict.parse(raw_xml)
+            vp = VulnerabilityProfile.create_from_xmldict(xmldict)
+            vuln_profiles.update({vp.name: vp})
+
+        profile_groups = {}
+        for profile_group in dg.findall("./profile-group/entry"):
+            raw_xml = ElementTree.tostring(profile_group)
+            xmldict = xmltodict.parse(raw_xml)
+            pg = SecurityProfileGroup.create_from_xmldict(xmldict)
+            profile_groups.update({pg.name: pg})
+
+        rules = []
+        for rule in dg.findall("./pre-rulebase/security/rules/entry"):
+            raw_xml = ElementTree.tostring(rule)
+            xmldict = xmltodict.parse(raw_xml)
             sr = SecurityRule.create_from_xmldict(xmldict)
             rules.append(sr)
 
-        return rules
+        return DeviceGroup(name, rules, vuln_profiles, profile_groups)
--- tests/panorama/test_panorama.py
+++ tests/panorama/test_panorama.py
@@ -1,104 +1,97 @@
-import xmltodict
-
-from panos_util.objects import SecurityProfileGroup, VulnerabilityProfile
 from panos_util.panorama import DeviceGroup
 
-RULES = """
-<rules>
-    <entry name="Strict">
-        <action>allow</action>
-        <profile-setting>
-            <profiles>
-                <vulnerability>
-                    <member>strict</member>
-                </vulnerability>
-            </profiles>
-        </profile-setting>
-    </entry>
-    <entry name="Default">
-        <action>allow</action>
-        <profile-setting>
-            <profiles>
-                <vulnerability>
-                    <member>default</member>
-                </vulnerability>
-            </profiles>
-        </profile-setting>
-    </entry>
-    <entry name="Disabled">
-        <action>allow</action>
-        <disabled>yes</disabled>
-    </entry>
-    <entry name="Drop">
-        <action>drop</action>
-    </entry>
-    <entry name="Alert-Only">
-        <action>allow</action>
-        <profile-setting>
-            <profiles>
-                <vulnerability>
-                    <member>Alert-Only</member>
-                </vulnerability>
-            </profiles>
-        </profile-setting>
-    </entry>
-    <entry name="Profile-Group">
-        <action>allow</action>
-        <profile-setting>
-            <group>
-                <member>Profile-Group</member>
-            </group>
-        </profile-setting>
-    </entry>
-</rules>
-"""
-
-ALERT_ONLY = """
-<entry name="Alert-Only">
-    <rules>
-        <entry name="Alert-All">
-            <action>
-                <alert/>
-            </action>
-            <vendor-id>
-                <member>any</member>
-            </vendor-id>
-            <severity>
-                <member>any</member>
-            </severity>
-            <cve>
-                <member>any</member>
-            </cve>
-            <threat-name>any</threat-name>
-            <host>any</host>
-            <category>any</category>
-            <packet-capture>disable</packet-capture>
+DEVICE_GROUP = """
+<entry name="Device-Group">
+    <profiles>
+        <vulnerability>
+            <entry name="Alert-Only">
+                <rules>
+                    <entry name="Alert-All">
+                        <action>
+                            <alert/>
+                        </action>
+                        <vendor-id>
+                            <member>any</member>
+                        </vendor-id>
+                        <severity>
+                            <member>any</member>
+                        </severity>
+                        <cve>
+                            <member>any</member>
+                        </cve>
+                        <threat-name>any</threat-name>
+                        <host>any</host>
+                        <category>any</category>
+                        <packet-capture>disable</packet-capture>
+                    </entry>
+                </rules>
+            </entry>
+        </vulnerability>
+    </profiles>
+    <profile-group>
+        <entry name="Profile-Group">
+            <vulnerability>
+                <member>strict</member>
+            </vulnerability>
         </entry>
-    </rules>
-</entry>
-"""
-
-PROFILE_GROUP = """
-<entry name="Profile-Group">
-    <vulnerability>
-        <member>strict</member>
-    </vulnerability>
+    </profile-group>
+    <pre-rulebase>
+        <security>
+            <rules>
+                <entry name="Strict">
+                    <action>allow</action>
+                    <profile-setting>
+                        <profiles>
+                            <vulnerability>
+                                <member>strict</member>
+                            </vulnerability>
+                        </profiles>
+                    </profile-setting>
+                </entry>
+                <entry name="Default">
+                    <action>allow</action>
+                    <profile-setting>
+                        <profiles>
+                            <vulnerability>
+                                <member>default</member>
+                            </vulnerability>
+                        </profiles>
+                    </profile-setting>
+                </entry>
+                <entry name="Disabled">
+                    <action>allow</action>
+                    <disabled>yes</disabled>
+                </entry>
+                <entry name="Drop">
+                    <action>drop</action>
+                </entry>
+                <entry name="Alert-Only">
+                    <action>allow</action>
+                    <profile-setting>
+                        <profiles>
+                            <vulnerability>
+                                <member>Alert-Only</member>
+                            </vulnerability>
+                        </profiles>
+                    </profile-setting>
+                </entry>
+                <entry name="Profile-Group">
+                    <action>allow</action>
+                    <profile-setting>
+                        <group>
+                            <member>Profile-Group</member>
+                        </group>
+                    </profile-setting>
+                </entry>
+            </rules>
+        </security>
+    </pre-rulebase>
 </entry>
 """
 
 
 def test_device_group():
-    rules = DeviceGroup.parse_rules(RULES)
-
-    vp_dict = xmltodict.parse(ALERT_ONLY)
-    vuln_profiles = {"Alert-Only": VulnerabilityProfile.create_from_xmldict(vp_dict)}
-
-    pg_dict = xmltodict.parse(PROFILE_GROUP)
-    profile_groups = {
-        "Profile-Group": SecurityProfileGroup.create_from_xmldict(pg_dict)
-    }
-
-    dg = DeviceGroup(rules, vuln_profiles, profile_groups)
+    dg = DeviceGroup.create_from_xml(DEVICE_GROUP)
     rule_counts = dg.rule_counts
 
     assert rule_counts["total"] == 6
