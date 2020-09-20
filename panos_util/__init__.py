391158ea751e9f4d881a678202431ace06d903df
Michael Richardson <mrichardson03@gmail.com>
2020-09-19 21:23:50 -0400

    style: Fix new black formatting

--- panos_util/__init__.py
+++ panos_util/__init__.py
@@ -2,11 +2,11 @@ __version__ = "0.1.0"
 
 
 def strip_empty(s: str) -> str:
-    """ If s is empty, return None.
+    """If s is empty, return None.
 
-        This is needed because getting the text attribute for an XML element returns
-        an empty string if the element is empty.  For example, the text attribute of
-        XML element '<element/>' will be the empty string, not None.
+    This is needed because getting the text attribute for an XML element returns
+    an empty string if the element is empty.  For example, the text attribute of
+    XML element '<element/>' will be the empty string, not None.
     """
     if s == "":
         return None
